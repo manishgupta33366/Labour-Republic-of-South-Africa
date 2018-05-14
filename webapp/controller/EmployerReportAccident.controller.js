b44c1873feb9e054eda9110b9526b1129a2d2cd8
S0019013022 <manish.g@ngahr.com>
2018-05-14 13:00:45 +0000

    SemiFinal Commit

--- webapp/controller/EmployerReportAccident.controller.js
+++ webapp/controller/EmployerReportAccident.controller.js
@@ -1,16 +1,127 @@
 sap.ui.define([
 	"com/employerreportaccident/controller/BaseController",
 	'sap/ui/model/json/JSONModel'
-], function(BaseController,JSONModel) {
+], function(BaseController, JSONModel) {
 	"use strict";
-
 	return BaseController.extend("com.employerreportaccident.controller.Dashboard", {
-		onInit: function () {
+		onInit: function() {
+			this._backendCall("/destination/services/PDFBuilder/values/Doc1", "GET", "", "");
+			//this._backendCall("/destination/services/PDFBuilder/DefaultValues","GET","","");
 			// set explored app's demo model on this sample
 			var oTableModel = new JSONModel(jQuery.sap.getModulePath("com.employerreportaccident.jsonData", "/EarningsOfEmployee.json"));
 			oTableModel.setDefaultBindingMode("TwoWay");
-			//var oTableModel = this.models.createModel(jQuery.sap.getModulePath("com.employerreportaccident.jsonData", "/EarningsOfEmployee.json"));
-			this.getView().setModel(oTableModel);
+			this.getView().setModel(oTableModel, "tablesData");
+		},
+		_defaultValues:{},
+		_backendCall: function(url, type, input, paramters) {
+			var self = this;
+			//var oBundle = this.getResourceBundle();
+			$.ajax({
+				url: url,
+				contentType: "application/json",
+				type: type,
+				data: JSON.stringify(input),
+				success: function(data) {
+					self._defaultValues=data;
+					var defaultValuesModel = new sap.ui.model.json.JSONModel(JSON.parse(data));
+					defaultValuesModel.setDefaultBindingMode("TwoWay");
+					self.getView().setModel(defaultValuesModel, "employerReportAccidentModel");
+					self.getView().bindElement("employerReportAccidentModel>/");
+					console.log(JSON.parse(data));
+				}
+			});
+		},
+		onSelectChangeInjuredRadioButtonGroup: function(oEvent) {
+			var modelData = this.getView().getModel("employerReportAccidentModel").getData();
+			modelData[12].value = oEvent.getSource().getButtons()[oEvent.getParameter("selectedIndex")].getText();
+			this.getView().getModel("employerReportAccidentModel").setData(modelData);
+		},
+		_updateModel: function(objectNumber, value) {
+			var modelData = this.getView().getModel("employerReportAccidentModel").getData();
+			modelData[objectNumber].value = value;
+			this.getView().getModel("employerReportAccidentModel").setData(modelData);
+		},
+		onSelectionChangeSegmentedButton: function(oEvent) {
+			if (oEvent.getSource().getId().indexOf("segmentedButtonSexID") !== -1) {
+				this._updateModel(17, oEvent.getParameters().item.getProperty("text"));
+			} else if (oEvent.getSource().getId().indexOf("segmentedButtonMartialStateID") !== -1) {
+				this._updateModel(18, oEvent.getParameters().item.getProperty("text"));
+			} else if (oEvent.getSource().getId().indexOf("segmentedButtonExpectedPeriodID") !== -1) {
+				this._updateModel(28, oEvent.getParameters().item.getProperty("text"));
+			} else if (oEvent.getSource().getId().indexOf("segmentedButtonAccidentWithTradeID") !== -1) {
+				this._updateModel(37, oEvent.getParameters().item.getProperty("text"));
+			} else if (oEvent.getSource().getId().indexOf("segmentedButtonMarkApplicable1ID") !== -1) {
+				this._updateModel(40.1, oEvent.getParameters().item.getProperty("text"));
+			} else if (oEvent.getSource().getId().indexOf("segmentedButtonTraficAccidentID") !== -1) {
+				this._updateModel(39, oEvent.getParameters().item.getProperty("text"));
+			} else if (oEvent.getSource().getId().indexOf("segmentedButtonSatisfiedID") !== -1) {
+				this._updateModel(41, oEvent.getParameters().item.getProperty("text"));
+			} else if (oEvent.getSource().getId().indexOf("segmentedButtonFurtherCompensationID") !== -1) {
+				this._updateModel(44, oEvent.getParameters().item.getProperty("text"));
+			} else if (oEvent.getSource().getId().indexOf("segmentedButtonShiftCompleteID") !== -1) {
+				this._updateModel(50, oEvent.getParameters().item.getProperty("text"));
+			} else if (oEvent.getSource().getId().indexOf("segmentedButtonFirstAidGIvenID") !== -1) {
+				this._updateModel(55, oEvent.getParameters().item.getProperty("text"));
+			} else if (oEvent.getSource().getId().indexOf("segmentedButtonActionWhileDrugsUnderInfluenceID") !== -1) {
+				this._updateModel(58.3, oEvent.getParameters().item.getProperty("text"));
+			} else if (oEvent.getSource().getId().indexOf("segmentedButtonActionNonComplianceWithDirectionsID") !== -1) {
+				this._updateModel(58.1, oEvent.getParameters().item.getProperty("text"));
+			} else if (oEvent.getSource().getId().indexOf("segmentedButtonRecklessDisregardID") !== -1) {
+				this._updateModel(58.2, oEvent.getParameters().item.getProperty("text"));
+			}
+		},
+		handleChangeDateTimePicker: function(oEvent){
+			if (oEvent.getSource().getId().indexOf("dateTimePickerAccident") !== -1) {
+				var dateTimeArray=oEvent.getParameter("newValue").split("-");
+				var date = new Date(dateTimeArray[0], dateTimeArray[1], dateTimeArray[2], dateTimeArray[3], dateTimeArray[4], dateTimeArray[5]).toISOString();
+				dateTimeArray=date.split("T");
+				this._updateModel(29, dateTimeArray[0]);
+				this._updateModel(30, dateTimeArray[1]);
+			}
+
+		},
+		onTableInputBoxChange: function(oEvent){
+			this._updateModel(oEvent.getSource().getBindingContext("tablesData").getProperty().R_Month_ID,oEvent.getSource().getBindingContext("tablesData").getProperty().R_Month);
+			this._updateModel(oEvent.getSource().getBindingContext("tablesData").getProperty().R_Week_ID,oEvent.getSource().getBindingContext("tablesData").getProperty().R_Week);
+		},
+		onCheckBoxSelected: function(oEvent){
+			this._updateModel(oEvent.getSource().getBindingContext("tablesData").getProperty().ID,oEvent.getParameter("selected"));
+		},
+		onPressSubmit: function() {
+
+		},
+		onPressReset: function() {
+			var _that = this;
+			var dialog = new sap.m.Dialog({
+				title: 'Confirm',
+				type: 'Message',
+				content: new sap.m.Text({
+					text: 'Are you sure you want to Reset this form?'
+				}),
+				beginButton: new sap.m.Button({
+					text: 'Yes',
+					press: function() {
+						var tableData = jQuery.sap.getModulePath("com.employerreportaccident.jsonData", "/EarningsOfEmployee.json");
+						_that.getView().getModel("tablesData").loadData(tableData);
+						_that.getView().getModel("employerReportAccidentModel").setData(JSON.parse(_that._defaultValues));
+						//_that.getView().bindElement("employerReportAccidentModel>/");
+						_that.getView().byId("ObjectPageLayoutOne").rerender();
+						sap.m.MessageToast.show('Form reset sucessfull!');
+						dialog.close();
+					}
+				}),
+				endButton: new sap.m.Button({
+					text: 'No',
+					press: function() {
+						dialog.close();
+					}
+				}),
+				afterClose: function() {
+					dialog.destroy();
+				}
+			});
+			dialog.open();
+
 		}
 	});
 });
--- webapp/utils/formatter.js
+++ webapp/utils/formatter.js
@@ -1,8 +1,110 @@
 sap.ui.define([],
-	function () {
+	function() {
 		"use strict";
 		return {
-						
+			ExpectedPeriodSegmentedButtonSelectedKey: function(value) {
+				var tag = {
+					"0-13 days": "SegmentButton0-13Days",
+					"14 & more": "satisfiedSegmentButton14OrMore"
+				};
+				return tag[value];
+			},
+			injuredRadioButtonGroupSelectIndex: function(value) {
+				var tag = {
+					"working director": 0,
+					"working member of a CC": 1,
+					"owner of": 2,
+					"partner in the business?": 3,
+					"Not applicable": 4
+				};
+				return tag[value];
+			},
+			SexSegmentedButtonSelectedKey: function(value) {
+				var tag = {
+					"Male": "SegmentButtonMale",
+					"Female": "SegmentButtonFeMale"
+				};
+				return tag[value];
+				//return ((tag[value]===null) || (tag[value]===undefined) || (tag[value]==="")  ? -1 : tag[value]);
+			},
+			MartialStateSegmentedButtonSelectedKey: function(value) {
+				var tag = {
+					"Married": "SegmentButtonMarried",
+					"Single": "SegmentButtonSingle"
+				};
+				return tag[value];
+			},
+			SegmentedButtonSelectedKeyAccidentWithTrade: function(value) {
+				var tag = {
+					"Yes": "accidentWithTradeSegmentButtonYes",
+					"No": "accidentWithTradeSegmentButtonNo"
+				};
+				return tag[value];
+			},
+			SegmentedButtonSelectedKeyInjuryNature: function(value) {
+				var tag = {
+					"Killed": "SegmentButtonKilled",
+					"Amputation": "SegmentButtonAmputation",
+					"Unconsciousness": "SegmentButtonUnconsciousness"
+				};
+				return tag[value];
+			},
+			SegmentedButtonSelectedKeyTraficAccident: function(value) {
+				var tag = {
+					"Yes": "traficAccidentSegmentButtonYes",
+					"No": "traficAccidentSegmentButtonNo"
+				};
+				return tag[value];
+			},
+			SegmentedButtonSelectedKeySatisfied: function(value) {
+				var tag = {
+					"Yes": "satisfiedSegmentButtonYes",
+					"No": "satisfiedSegmentButtonNo"
+				};
+				return tag[value];
+			},
+			SegmentedButtonSelectedKeyFurtherCompensation: function(value) {
+				var tag = {
+					"Yes": "furtherCompensationSegmentButtonYes",
+					"No": "furtherCompensationSegmentButtonNo"
+				};
+				return tag[value];
+			},
+			SegmentedButtonSelectedKeyShiftComplete: function(value) {
+				var tag = {
+					"Yes": "ShiftCompleteSegmentButtonYes",
+					"No": "ShiftCompleteSegmentButtonNo"
+				};
+				return tag[value];
+			},
+			SegmentedButtonSelectedKeyFirstAidGIven: function(value) {
+				var tag = {
+					"Yes": "FirstAidGIvenSegmentButtonYes",
+					"No": "FirstAidGIvenSegmentButtonNo"
+				};
+				return tag[value];
+			},
+			SegmentedButtonSelectedKeyActionWhileDrugs: function(value) {
+				var tag = {
+					"Yes": "ActionWhileDrugsUnderInfluenceSegmentButtonYes",
+					"No": "ActionWhileDrugsUnderInfluenceSegmentButtonNo"
+				};
+				return tag[value];
+			},
+			SegmentedButtonSelectedKeynonCompliance: function(value) {
+				var tag = {
+					"Yes": "nonComplianceWithDirectionsSegmentButtonYes",
+					"No": "nonComplianceWithDirectionsSegmentButtonNo"
+				};
+				return tag[value];
+			},
+			SegmentedButtonSelectedKeyrecklessDisregard: function(value) {
+				var tag = {
+					"Yes": "recklessDisregardSegmentButtonYes",
+					"No": "recklessDisregardSegmentButtonNo"
+				};
+				return tag[value];
+			}
 		};
-		
+
 	});
